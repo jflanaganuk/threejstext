37ee78d7e49d24e7594908392b5ecbfde43a5c2a
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-05-29 22:49:31 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from 'react-three-fiber';
+import * as THREE from 'three';
+import fira from './Fira.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Canvas>
+        <mesh>
+          <textGeometry attach='geometry' args={[
+            'hello world',
+            {
+              font: new THREE.FontLoader().parse(fira),
+              size: 5,
+              height: 1,
+            }
+          ]}/>
+          <meshPhongMaterial/>
+        </mesh>
+      </Canvas>
   );
 }
 
