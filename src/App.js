eaba6e38d070e3ad93f24d8f4534cabed28f7eff
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-05-29 23:13:09 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Canvas } from 'react-three-fiber';
 import * as THREE from 'three';
 import fira from './Fira.json'
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
       <Canvas style={{
         width: '100vw',
-        height: '100vh'
+        height: '100vh',
+        backgroundColor: 'black'
       }}>
+        <OrbitControls/>
         <pointLight/>
         <mesh position={[-25, -1, -20]}>
           <textGeometry attach='geometry' args={[
@@ -18,7 +21,7 @@ function App() {
               height: 1,
             }
           ]}/>
-          <meshPhongMaterial/>
+          <meshNormalMaterial/>
         </mesh>
       </Canvas>
   );
